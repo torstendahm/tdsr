8886d0a9c257d26f57421edbf6a69903e767d188
torstendahm <dahm@gfz-potsdam.de>
2023-12-26 18:53:03 +0100

    Bump version: 0.0.7 → 0.0.8

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ author = "Torsten Dahm, Roman Dahm, Sebastian Hainzl"
 
 # The full version, including alpha/beta/rc tags
 # never edit this manually!
-release = "0.0.7"
+release = "0.0.8"
 
 
 # -- General configuration ---------------------------------------------------
